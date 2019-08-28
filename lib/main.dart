f1008d7d4df4456dbd09ffc1c76ca7cc75882c7f
yoridewit@gmail.com <yoridewit@gmail.com>
2019-08-28 08:32:50 +0200

    -

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import './quiz.dart';
 import './result.dart';
 
+//For branch test
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
