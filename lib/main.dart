82ce8093efa4cff38ec48a0d0bbc28007c50abf5
Yori <yoridewit@gmail.com>
2019-08-28 06:44:53 +0200

    first_test

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  final questions = const [
+    {
+      'questionText': 'Whats your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'Whats your favorite animal?',
+      'answers': ['Sheep', 'Dog', 'Cat', 'Whale'],
+    },
+    {
+      'questionText': 'Whats your favorite food?',
+      'answers': ['Beans', 'Banana', 'Apple', 'Pie'],
+    },
+  ];
+
+  void _answerQuestion() {
+    if (_questionIndex < questions.length) {}
+
+    setState(() {
+      _questionIndex++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex]['questionText'],
+            ),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
+          ],
+        ),
+      ),
+    );
+  }
+}
